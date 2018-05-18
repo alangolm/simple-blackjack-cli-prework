ffe008ee43348065d9eb928bfa3f7443336ef2a7
Alan Golman <golman.alan@gmail.com>
2018-05-18 16:49:16 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,10 +50,10 @@ end
 def runner
   welcome 
   initial_round
-  hit?(card_total)
-  display_card_total(card_total)
   until card_total > 21 
-    
+    hit?(card_total)
+    display_card_total(card_total)
   end
+end_game(card_total)
 end
     
